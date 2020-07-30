b6ccbdcfb86b1d84bf61b734045199190d707440
hhkp <s.volodymyr82@gmail.com>
2020-07-30 20:23:46 +0300

    added build

---
+++ build/precache-manifest.640c3f7334eeb27de64dfd40e8a6aa63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4904b1a9ba761d2b5e86b444fa823105",
+    "url": "/amc/index.html"
+  },
+  {
+    "revision": "bf75cac4de4aa1db0952",
+    "url": "/amc/static/css/2.d71805a1.chunk.css"
+  },
+  {
+    "revision": "700ed028684d549b711c",
+    "url": "/amc/static/css/main.63c2de36.chunk.css"
+  },
+  {
+    "revision": "bf75cac4de4aa1db0952",
+    "url": "/amc/static/js/2.c2cb756b.chunk.js"
+  },
+  {
+    "revision": "700ed028684d549b711c",
+    "url": "/amc/static/js/main.478f11ba.chunk.js"
+  },
+  {
+    "revision": "eee39aee8c751f8fc936",
+    "url": "/amc/static/js/runtime-main.b40af11b.js"
+  },
+  {
+    "revision": "2b6a833a8a3c06283d938b987027221b",
+    "url": "/amc/static/media/wood-bg-1440x900.2b6a833a.png"
+  }
+]);
